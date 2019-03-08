2cf3a057793accf58f89e4ddca5789cf807065c0
Valery Strelets <brooons@yandex.ru>
2019-03-09 00:30:31 +0200

    start changes to webpack

--- src/timezz.js
+++ src/timezz.js
@@ -1,97 +1,86 @@
 /*!
- * TimezZ v4.0.1: Plugin for countdown and count forward
+ * TimezZ v5.0.0: Plugin for countdown and count forward
  *
  * Contribute: https://github.com/BrooonS/TimezZ
  * Released under the MIT license: http://opensource.org/licenses/MIT
  */
 
-(() => {
-  class TimezZ {
-    constants = {
-      ONE_SECOND: 1000,
-      ONE_MINUTE: 1000 * 60,
-      ONE_HOUR: 1000 * 60 * 60,
-      ONE_DAY: 1000 * 60 * 60 * 24,
+const ONE_SECOND = 1000;
+const ONE_MINUTE = ONE_SECOND * 60;
+const ONE_HOUR = ONE_MINUTE * 60;
+const ONE_DAY = ONE_HOUR * 24;
+
+export default class TimezZ {
+  constructor(selector, userSettings = {}) {
+    if (selector === undefined) {
+      throw new Error('Selector isn\'t passed.');
     }
 
-    constructor(element, userSettings = {}) {
-      this.element = document.querySelector(element);
-      this.settings = {
-        date: 'Jan 01, 2040 00:00:00',
-        daysName: 'd',
-        hoursName: 'h',
-        minutesName: 'm',
-        secondsName: 's',
-        isStopped: false,
-        template: '<span>NUMBER<i>LETTER</i></span> ',
-        beforeCreate() {},
-        ...userSettings,
-      };
+    this.element = document.querySelector(selector);
+    this.settings = {
+      date: 'Jan 01, 2040 00:00:00',
+      daysName: 'd',
+      hoursName: 'h',
+      minutesName: 'm',
+      secondsName: 's',
+      isStopped: false,
+      template: '<span>NUMBER<i>LETTER</i></span> ',
+      beforeCreate() {},
+      ...userSettings,
+    };
 
-      this.initTimer();
+    this.initTimer();
+  }
+
+  initTimer() {
+    const {
+      date,
+      daysName,
+      hoursName,
+      isStopped,
+      minutesName,
+      secondsName,
+      beforeCreate,
+    } = this.settings;
+
+    const countDate = new Date(date).getTime();
+    const currentTime = new Date().getTime();
+    const distance = countDate - currentTime;
+
+    const calculateTemplate = (math) => {
+      const fixNumber = number => (number >= 10 ? number : `0${number}`);
+
+      return fixNumber(Math.floor(Math.abs(math)));
+    };
+
+    /**
+     * Hard math.
+     */
+    const countDays = calculateTemplate(distance / ONE_DAY);
+    const countHours = calculateTemplate((distance % ONE_DAY) / ONE_HOUR);
+    const countMinutes = calculateTemplate((distance % ONE_HOUR) / ONE_MINUTE);
+    const countSeconds = calculateTemplate((distance % ONE_MINUTE) / ONE_SECOND);
+
+    /**
+     * User callback.
+     */
+    if (typeof beforeCreate === 'function') {
+      beforeCreate();
     }
 
-    initTimer() {
-      const {
-        date,
-        daysName,
-        hoursName,
-        isStopped,
-        minutesName,
-        secondsName,
-        beforeCreate,
-      } = this.settings;
+    this.element.innerHTML = (
+      this.outputTemplate(countDays, daysName)
+      + this.outputTemplate(countHours, hoursName)
+      + this.outputTemplate(countMinutes, minutesName)
+      + this.outputTemplate(countSeconds, secondsName)
+    );
 
-      const {
-        ONE_SECOND,
-        ONE_MINUTE,
-        ONE_HOUR,
-        ONE_DAY,
-      } = this.constants;
-
-      const countDate = new Date(date).getTime();
-      const currentTime = new Date().getTime();
-      const distance = countDate - currentTime;
-
-      /**
-       * Hard math.
-       */
-      const countDays = this.calculateTemplate(distance / ONE_DAY);
-      const countHours = this.calculateTemplate((distance % ONE_DAY) / ONE_HOUR);
-      const countMinutes = this.calculateTemplate((distance % ONE_HOUR) / ONE_MINUTE);
-      const countSeconds = this.calculateTemplate((distance % ONE_MINUTE) / ONE_SECOND);
-
-      /**
-       * User callback.
-       */
-      if (typeof beforeCreate === 'function') {
-        beforeCreate();
-      }
-
-      this.element.innerHTML = (
-        this.outputTemplate(countDays, daysName)
-        + this.outputTemplate(countHours, hoursName)
-        + this.outputTemplate(countMinutes, minutesName)
-        + this.outputTemplate(countSeconds, secondsName)
-      );
-
-      if (!isStopped) {
-        setTimeout(this.initTimer.bind(this), ONE_SECOND);
-      }
-    }
-
-    fixNumber = number => (number >= 10 ? number : `0${number}`);
-
-    calculateTemplate = math => this.fixNumber(Math.floor(Math.abs(math)));
-
-    outputTemplate(number, letter) {
-      return this.settings.template.replace(/NUMBER/g, number).replace(/LETTER/g, letter);
+    if (!isStopped) {
+      setTimeout(this.initTimer.bind(this), ONE_SECOND);
     }
   }
 
-  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = TimezZ;
-  } else {
-    window.TimezZ = TimezZ;
+  outputTemplate(number, letter) {
+    return this.settings.template.replace(/NUMBER/g, number).replace(/LETTER/g, letter);
   }
-})();
+}
